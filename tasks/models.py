a5e7e392f3f2f6788438c9b8996d8484895d852d
Tolu Joel <toluisjoel@gmail.com>
2023-01-19 12:16:26 +0100

    Added Task list template and styling

--- tasks/models.py
+++ tasks/models.py
@@ -4,21 +4,24 @@ from django.contrib.auth import get_user_model
 
 class Task(models.Model):
     STATUS_CHOICES = (
-        ('on hold', 'on hold'),
+        ('not-started', 'not-started'),
+        ('in-progress', 'in-progress'),
+        ('in-review', 'in-review'),
+        ('suspended', 'suspended'),
         ('completed', 'completed'),
-        ('in review', 'in review'),
-        ('not started', 'not started'),
-        ('in progress', 'in progress'),
     )
     name = models.CharField(max_length=255)
     description = models.TextField()
-    due_date = models.DateTimeField()
+    due_date = models.DateField()
     assignee = models.ForeignKey(
         get_user_model(),
         related_name='assignee',
         on_delete=models.CASCADE,
     )
     status = models.CharField(max_length=20, choices=STATUS_CHOICES)
+    
+    class Meta:
+        ordering = ('due_date',)
 
-    def __str__(self) -> str:
+    def __str__(self):
         return self.name
