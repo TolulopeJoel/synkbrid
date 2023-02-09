23e02f745ae0796a43fa14cc79c6a8edab1670eb
Tolu Joel <toluisjoel@gmail.com>
2023-02-10 00:31:03 +0100

    added external database

--- RemoteCollaborate/settings.py
+++ RemoteCollaborate/settings.py
@@ -78,14 +78,12 @@ TEMPLATES = [
 WSGI_APPLICATION = 'RemoteCollaborate.wsgi.application'
 
 
+import dj_database_url
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.config()
 }
 
 
