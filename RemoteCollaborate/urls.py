83111a86999e817cf5f518b7066ff95a74a97642
Tolu Joel <toluisjoel@gmail.com>
2023-02-09 23:59:22 +0100

    removed redundant import

--- RemoteCollaborate/urls.py
+++ RemoteCollaborate/urls.py
@@ -18,8 +18,6 @@ from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import include, path
 
-from . import consumers
-
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('tasks.urls')),
