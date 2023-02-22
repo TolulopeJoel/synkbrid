01aac8ee87062d94e85c89dfedbe9f797de3e664
Tolu Joel <toluisjoel@gmail.com>
2023-02-22 21:04:15 +0100

    Added AssigneePublicSerializer to TaskSerializer

--- tasks/serializers.py
+++ tasks/serializers.py
@@ -3,15 +3,23 @@ from rest_framework import serializers
 from .models import Task
 
 
+class AssigneePublicSerializer(serializers.Serializer):
+    id = serializers.IntegerField(read_only=True)
+    username = serializers.CharField(read_only=True)
+
+
 class TaskSerializer(serializers.ModelSerializer):
+    assignee = AssigneePublicSerializer(read_only=True)
+    
     class Meta:
         model = Task
         fields = [
             'id',
+            'assignee',
             'name',
             'description',
             'start_date',
             'due_date',
-            'assignee',
             'status',
         ]
+        
