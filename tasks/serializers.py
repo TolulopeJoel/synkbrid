d1879db20d2b330242665cfdf9c050e5a2709ffb
Tolu Joel <toluisjoel@gmail.com>
2023-03-22 18:05:41 +0100

    Removed team from task serializer

--- tasks/serializers.py
+++ tasks/serializers.py
@@ -2,18 +2,14 @@ from rest_framework import serializers
 
 from .models import Task
 
-from accounts.serializers import TeamSerializer
-
 
 class TaskSerializer(serializers.ModelSerializer):
-    team = TeamSerializer(read_only=True)
     
     class Meta:
         model = Task
         fields = [
             'id',
             'name',
-            'team',
             'description',
             'start_date',
             'due_date',
