ad459f202e999837efed5f927a4d70253829cb8d
Tolu Joel <toluisjoel@gmail.com>
2023-04-27 15:11:21 +0100

    Added new url to cors whitelist

--- RemoteCollaborate/settings.py
+++ RemoteCollaborate/settings.py
@@ -137,8 +137,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = '/static/'
-STATICFILES_DIRS = [BASE_DIR / 'static']
-STATIC_ROOT = BASE_DIR / 'staticfiles'
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedStaticFilesStorage'
 
 MEDIA_URL = 'media/'
@@ -174,5 +172,6 @@ SIMPLE_JWT = {
 }
 
 CORS_ORIGIN_WHITELIST = [
-     'http://localhost:3000'
+     'http://localhost:3000',
+     'https://rc-prj-mng.netlify.app',
 ]
